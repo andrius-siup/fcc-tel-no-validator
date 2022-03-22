a60b31f2964af3c06f52654ea371aa83c6caeb79
andrius-siup <andrius.siupinys@gmail.com>
2022-03-22 12:32:05 +0000

    Create regex that will pass '555 555 5555', also create regex array and loop them in the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 const RE = /^\d{3}-\d{3}-\d{4}$/; // passed 555-555-5555
+
 // '\(' match open brackets and match the close brackets '\)'
+// passed the pattern (555)555-5555 and (555) 555-5555
 const RE2 = /^\(\d{3}\)\s?\d{3}-\d{4}$/;
+// passed "555 555 5555"
+const RE3 = /^\d{3}\s\d{3}\s\d{4}$/;
+
+// Create array of regexes
+const REGEXES = [RE, RE2, RE3];
+
 function telephoneCheck(str) {
-  return RE.test(str) || RE2.test(str);
+  // Loop through regexes
+  for (let regex of REGEXES) {
+    // if one matches, return true
+    if (regex.test(str)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 console.log(telephoneCheck("555-555-5555"));
 console.log(telephoneCheck("(555)555-5555"));
 console.log(telephoneCheck("(555) 555-5555"));
+console.log(telephoneCheck("555 555 5555"));
