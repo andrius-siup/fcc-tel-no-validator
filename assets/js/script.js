dff32408c405839e000a2f02437b540424879a2a
andrius-siup <andrius.siupinys@gmail.com>
2022-03-22 12:19:22 +0000

    Create regex that will match the pattern (555)555 5555

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-const RE = /^\d{3}-\d{3}-\d{4}$/;
-
+const RE = /^\d{3}-\d{3}-\d{4}$/; // passed 555-555-5555
+// '\(' match open brackets and match the close brackets '\)'
+const RE2 = /^\(\d{3}\)\d{3}-\d{4}$/;
 function telephoneCheck(str) {
-  return RE.test(str);
+  return RE.test(str) || RE2.test(str);
 }
 
 console.log(telephoneCheck("555-555-5555"));
-console.log(telephoneCheck("555-555-5555lalal"));
+console.log(telephoneCheck("(555)555-5555"));
