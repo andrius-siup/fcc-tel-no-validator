2693598a1c7600f1a68b6f4f18d24670294b3f13
andrius-siup <andrius.siupinys@gmail.com>
2022-03-22 14:33:53 +0000

    Simplified - added group regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
-const RE = /^\d{3}-\d{3}-\d{4}$/; // passed 555-555-5555
+const RE = /^(1\s?)?\d{3}-\d{3}-\d{4}$/; // passed 555-555-5555
 
 // '\(' match open brackets and match the close brackets '\)'
 // passed the pattern (555)555-5555 and (555) 555-5555
-const RE2 = /^\(\d{3}\)\s?\d{3}-\d{4}$/;
+// (1\s?)? - grouped maybe 1 maybe space
+const RE2 = /^(1\s?)?\(\d{3}\)\s?\d{3}-\d{4}$/;
 // passed "555 555 5555" and "5555555555"
-const RE3 = /^\d{3}\s?\d{3}\s?\d{4}$/;
+const RE3 = /^(1\s?)?\d{3}\s?\d{3}\s?\d{4}$/;
 // passed "1 555 555 5555"
-const RE4 = /^1\s\d{3}\s\d{3}\s\d{4}$/;
+// const RE4 = /^1\s\d{3}\s\d{3}\s\d{4}$/;
 // passed "1 555-555-5555"
-const RE5 = /^1\s\d{3}-\d{3}-\d{4}$/;
+// const RE5 = /^1\s\d{3}-\d{3}-\d{4}$/;
 // passed "1 (555) 555-5555" "1 (555)555-5555" "1(555)555-5555"
-const RE6 = /^1\s?\(\d{3}\)\s?\d{3}-\d{4}$/;
+// const RE6 = /^1\s?\(\d{3}\)\s?\d{3}-\d{4}$/;
 // Create array of regexes
-const REGEXES = [RE, RE2, RE3, RE4, RE5, RE6];
+const REGEXES = [RE, RE2, RE3];
 
 function telephoneCheck(str) {
   // Loop through regexes
