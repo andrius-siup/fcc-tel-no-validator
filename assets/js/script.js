6c154a82e724f0da236aea0fa8061fb7b6b77912
andrius-siup <andrius.siupinys@gmail.com>
2022-03-22 12:21:01 +0000

    Update RE2 that will match the pattern with white space or not after closing bracket

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 const RE = /^\d{3}-\d{3}-\d{4}$/; // passed 555-555-5555
 // '\(' match open brackets and match the close brackets '\)'
-const RE2 = /^\(\d{3}\)\d{3}-\d{4}$/;
+const RE2 = /^\(\d{3}\)\s?\d{3}-\d{4}$/;
 function telephoneCheck(str) {
   return RE.test(str) || RE2.test(str);
 }
 
 console.log(telephoneCheck("555-555-5555"));
 console.log(telephoneCheck("(555)555-5555"));
+console.log(telephoneCheck("(555) 555-5555"));
