a9ea669ccd4d6b21b71b93fe7a14a02026ac71e7
andrius-siup <andrius.siupinys@gmail.com>
2022-03-22 12:09:53 +0000

    Simplified regex - same into 3 3 and 4 digit groups, also added start anchor - that will match only 3 digit numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-const RE = /\d\d\d-\d\d\d-\d\d\d\d/;
+const RE = /^\d{3}-\d{3}-\d{4}/;
 
 function telephoneCheck(str) {
   return RE.test(str);
 }
 
 console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("lalal555-555-5555"));
